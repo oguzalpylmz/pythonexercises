e7dc19ce382adae9991e613260dd7a4199d88138
Oğuzalp Yılmaz <125830569+oguzalpylmz@users.noreply.github.com>
2023-03-27 22:44:36 +0300

    Update sayi_tahmin_oyunu.py

--- sayi_tahmin_oyunu.py
+++ sayi_tahmin_oyunu.py
@@ -12,76 +12,36 @@ Tekrar Başlamak İçin:R
 
 rastgele_sayi = random.randint(1, 40)
 tahmin_hakkı = 5
-
 while True:
-    print("Tahmin Hakkı =", tahmin_hakkı," \n ")
-    tahmin = input("Tahmininizi girin:")
-    if str(tahmin) == "q":
-        print("Oyundan çıkılıyor....")
-        time.sleep(1)
-        break
-    elif tahmin_hakkı == 1:
-        print("Tahmin hakkınız kalmadı.\nSayınız:",rastgele_sayi,"\nKaybettiniz...")
-        time.sleep(1)
-        break
-    elif str(tahmin) == "l":
-        print("Sayı:",rastgele_sayi)
-        time.sleep(1)
-        print("Tekrar başlanıyor")
-        time.sleep(1)
-
-        rastgele_sayi = random.randint(1, 40)
-        tahmin_hakkı = 5
-    elif (int(tahmin) < rastgele_sayi):
-        print("Sayı Sorgulanıyor....")
-        time.sleep(1)
-        tahmin_hakkı -= 1
-        print("Lütfen daha yüksek bir sayı söyleyin.")
-        print("Tahmin Hakkı:", tahmin_hakkı)
-    elif (int(tahmin) > rastgele_sayi):
-        print("Sayı Sorgulanıyor....")
-        time.sleep(1)
-        tahmin_hakkı -= 1
-        print("Lütfen daha düşük bir sayı söyleyin.")
-        print("Tahmin Hakkı:", tahmin_hakkı)
-    elif str(tahmin) == "r":
-        print("Tekrar başlanıyor")
-        time.sleep(1)
-        rastgele_sayi = random.randint(1, 40)
-        tahmin_hakkı = 5
-
-    elif 0<int(tahmin)<40:
-
-        if rastgele_sayi==tahmin:
+    try:
+        print("Tahmin Hakkı =", tahmin_hakkı, " \n ")
+        tahmin = input("Tahmininizi girin:")
+        if str(tahmin) == "q":
+            print("Oyundan çıkılıyor....")
+            time.sleep(1)
+            break
+        elif str(tahmin) == "l":
+            print("Sayınız: "+ str(rastgele_sayi))
+            time.sleep(1)
+            break
+        elif not 1 < int(tahmin) < 40:
+            print("Geçerli aralıkta sayı giriniz.")
+        elif int(tahmin) == int(rastgele_sayi):
             print("Tebrikler doğru bildiniz.")
             time.sleep(1)
             break
-        else:
-            print("Tekrar deneyiniz")
+        elif tahmin_hakkı == 1:
+            print("Tahmin hakkınız kalmadı.\nSayınız:", rastgele_sayi, "\nKaybettiniz...")
             time.sleep(1)
+            break
+        elif int(tahmin) < int(rastgele_sayi):
+            print("daha büyük bir sayı gir")
             tahmin_hakkı -= 1
-    else:
-        print("Geçerli bir sayı giriniz.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        elif int(tahmin) > int(rastgele_sayi):
+            print("daha küçük sayı gir")
+            tahmin_hakkı -= 1
+        else:
+            print("Geçerli bir sayı giriniz.")
+            break
+    except:
+        print("Geçerli bir değer giriniz")
